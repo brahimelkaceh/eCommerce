04ccf8d9df9c93dc1aadc1b980afaf4c095148fd
brahimelkaceh <brahimelkaceh@gmail.com>
2023-10-30 22:30:59 +0100

    commit

--- backend/routes/categoriesRoutes.js
+++ backend/routes/categoriesRoutes.js
@@ -1,43 +1,24 @@
 // ! Express.js routes for categories
 const express = require("express");
 const Router = express.Router();
-<<<<<<< HEAD
-const { TokenCheck } = require("../middlewares/TokenCheck");
-const {
-  createCategory,
-  getAllCategories,
-  getCategoryById,
-} = require("../controllers/categoriesController");
-
-Router.post("/categories/", TokenCheck, createCategory);
-Router.get("/categories/", TokenCheck, getAllCategories);
-
-Router.get("/categories/:id", getCategoryById);
-
-=======
-
 
 const validatorSanitizer = require("../middlewares/validator");
 const { TokenCheck } = require("../middlewares/TokenCheck");
 
 const ValidatorSanitizer = new validatorSanitizer();
- const {searchCategory , createCategory,updateCategory,deleteCategory,showAllCategories,getCategoryById}=require('../controllers/categoriesController')
+const {
+  searchCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  showAllCategories,
+  getCategoryById,
+} = require("../controllers/categoriesController");
 
-
-
-
-
-
-
-
-
-
-
-Router.get("/categories", searchCategory) 
-Router.post("/categories",createCategory)
-Router.patch("/categories/:id",updateCategory)
-Router.delete("/categories/:id",deleteCategory)
-Router.get("/categories/allCategories",showAllCategories)
-Router.get("/categories/:id",getCategoryById)
->>>>>>> 23c7db98080b7a9dca2945c27c26115c031d49d6
+Router.get("/categories", TokenCheck, searchCategory);
+Router.post("/categories", TokenCheck, createCategory);
+Router.patch("/categories/:id", TokenCheck, updateCategory);
+Router.delete("/categories/:id", TokenCheck, deleteCategory);
+Router.get("/categories/allCategories", TokenCheck, showAllCategories);
+Router.get("/categories/:id", TokenCheck, getCategoryById);
 module.exports = Router;
--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,34 +1,8 @@
 // ! Express.js routes for products
-<<<<<<< HEAD
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productsController = require('../controllers/productsController');
+const productsController = require("../controllers/productsController");
 
-router.get('/products/:productId', productsController.getProductById) ;
+router.get("/products/:productId", productsController.getProductById);
 
 module.exports = router;
-=======
-const express = require("express");
-const Router = express.Router();
-
-
-const validatorSanitizer = require("../middlewares/validator");
-const { TokenCheck } = require("../middlewares/TokenCheck");
-
-const ValidatorSanitizer = new validatorSanitizer();
- const {searchproduct}=require('../controllers/productsController')
-
-
-
-
-
-
-
-
-
-
-
-Router.get("/products", searchproduct) 
-
-module.exports = Router;
->>>>>>> 23c7db98080b7a9dca2945c27c26115c031d49d6
