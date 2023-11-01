ba15be1f6ac1db057ab1a9fa3c640173fcf0b467
brahimelkaceh <brahimelkaceh@gmail.com>
2023-11-01 16:30:43 +0100

    commit hamid

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -9,11 +9,12 @@ const {
 } = require("../controllers/ordersController");
 //const validatorSanitizer = require("../middlewares/validator");
 //const ValidatorSanitizer = new validatorSanitizer();
-//const { TokenCheck } = require('../middlewares/TokenCheck');
+const { TokenCheck } = require("../middlewares/TokenCheck");
+const { CustomerTokenCheck } = require("../middlewares/CustomerTokenCheck");
 
-Router.post("/requests", createOrder);
-Router.get("/requests/:id", getOrderById);
-Router.put("/requests/:id", updateOrder);
-Router.get("/requests", listOrders);
+Router.post("/requests", CustomerTokenCheck, createOrder);
+Router.get("/requests/:id", CustomerTokenCheck, getOrderById);
+Router.put("/requests/:id", CustomerTokenCheck, updateOrder);
+Router.get("/requests", CustomerTokenCheck, listOrders);
 
 module.exports = Router;
