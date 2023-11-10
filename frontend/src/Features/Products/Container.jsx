0df92199ffc132b4df821faff9ad5b3f21391e38
brahimelkaceh <brahimelkaceh@gmail.com>
2023-11-10 10:45:26 +0100

    commit products container

---
+++ frontend/src/Features/Products/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = () => {
+  return <div>Container for products</div>;
+};
+
+export default Container;
--- frontend/src/Pages/products/Products.jsx
+++ frontend/src/Pages/products/Products.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { DrawerHeader } from "../../Components/mui/MuiStyles";
 import { Box } from "@mui/material";
 import Sidebar from "../../Components/sidebar/Sidebar";
+import Container from "../../Features/Products/Container";
 const Products = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <Sidebar />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <h4>Products</h4>
+        <Container />
       </Box>
     </Box>
   );
