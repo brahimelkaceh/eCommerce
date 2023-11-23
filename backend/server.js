0a255e1bcf7d44c078083d6ad08c8dc6cc0ed4d9
KayZou <elghazzalyzakaria@gmail.com>
2023-11-23 09:56:14 +0100

    backend jadid

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,12 @@ require("dotenv").config();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(cors());
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 server.use(morgan("dev"));
 
 server.use("/", userRouter);
